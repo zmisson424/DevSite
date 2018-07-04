8456da41cb32022ea7912ddef1ef2b5b2b46282c
Zachery Misson <zmisson424@gmail.com>
2018-07-04 14:30:30 -0400

    started adding layout to the Create page

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { colors } from '../../config';
+import React, { Component } from "react";
+import { colors } from "../../config";
 
 import {
   Drawer,
@@ -9,16 +9,16 @@ import {
   ListItemText,
   Avatar,
   Divider
-} from '@material-ui/core';
+} from "@material-ui/core";
 import {
   Dashboard as DashIcon,
   CloudUpload,
   Assignment,
   Help as HelpIcon
-} from '@material-ui/icons';
-import { withStyles } from '@material-ui/core/styles';
+} from "@material-ui/icons";
+import { withStyles } from "@material-ui/core/styles";
 
-import { Home, Create, Develop, Contact } from '../../pages';
+import { Home, Create, Develop, Contact } from "../../pages";
 
 const drawerWidth = 245;
 
@@ -26,29 +26,37 @@ const styles = theme => ({
   root: {
     flexGrow: 1,
     zIndex: 1,
-    overflow: 'hidden',
-    position: 'relative',
-    display: 'flex'
+    overflow: "hidden",
+    position: "relative",
+    display: "flex"
+  },
+  appFrame: {
+    height: '100%',
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
   },
   drawerPaper: {
-    position: 'relative',
+    position: "relative",
     width: drawerWidth,
     backgroundColor: colors.primaryDark
   },
   content: {
     flexGrow: 1,
-    backgroundColor: '#FAFAFA'
+    backgroundColor: "#FAFAFA"
   },
   avatar: {
     height: 200,
     width: 200,
-    marginLeft: '10%',
-    marginRight: '10%',
+    marginLeft: "10%",
+    marginRight: "10%",
     marginTop: 25,
     color: colors.secondary
   },
   item: {
-    color: '#ffffff'
+    color: "#ffffff"
   }
 });
 
@@ -69,92 +77,94 @@ class Layout extends Component {
 
     return (
       <div className={classes.root}>
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: classes.drawerPaper
-          }}
-        >
-          <div
-            style={{
-              marginBottom: 10
+        <div className={classes.appFrame}>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper
             }}
           >
-            <Avatar alt="Embark Development" className={classes.avatar}>
-              Embark Dev
-            </Avatar>
-            <Divider style={{ marginTop: 25 }} />
-          </div>
+            <div
+              style={{
+                marginBottom: 10
+              }}
+            >
+              <Avatar alt="Embark Development" className={classes.avatar}>
+                Embark Dev
+              </Avatar>
+              <Divider style={{ marginTop: 25 }} />
+            </div>
 
-          <List>
-            <ListItem button onClick={() => this.changePageNumber(1)}>
-              <ListItemIcon
-                classes={{
-                  root: classes.item
-                }}
-              >
-                <DashIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary="Home"
-                classes={{
-                  primary: classes.item
-                }}
-              />
-            </ListItem>
-            <ListItem button onClick={() => this.changePageNumber(2)}>
-              <ListItemIcon
-                classes={{
-                  root: classes.item
-                }}
-              >
-                <Assignment />
-              </ListItemIcon>
-              <ListItemText
-                primary="Create"
-                classes={{
-                  primary: classes.item
-                }}
-              />
-            </ListItem>
-            <ListItem button onClick={() => this.changePageNumber(3)}>
-              <ListItemIcon
-                classes={{
-                  root: classes.item
-                }}
-              >
-                <CloudUpload />
-              </ListItemIcon>
-              <ListItemText
-                primary="Develop"
-                classes={{
-                  primary: classes.item
-                }}
-              />
-            </ListItem>
-            <ListItem button onClick={() => this.changePageNumber(4)}>
-              <ListItemIcon
-                classes={{
-                  root: classes.item
-                }}
-              >
-                <HelpIcon />
-              </ListItemIcon>
-              <ListItemText
-                primary="Contact"
-                classes={{
-                  primary: classes.item
-                }}
-              />
-            </ListItem>
-          </List>
-        </Drawer>
-        <main className={classes.content}>
-          {pageNumber === 1 && <Home />}
-          {pageNumber === 2 && <Create />}
-          {pageNumber === 3 && <Develop />}
-          {pageNumber === 4 && <Contact />}
-        </main>
+            <List>
+              <ListItem button onClick={() => this.changePageNumber(1)}>
+                <ListItemIcon
+                  classes={{
+                    root: classes.item
+                  }}
+                >
+                  <DashIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Home"
+                  classes={{
+                    primary: classes.item
+                  }}
+                />
+              </ListItem>
+              <ListItem button onClick={() => this.changePageNumber(2)}>
+                <ListItemIcon
+                  classes={{
+                    root: classes.item
+                  }}
+                >
+                  <Assignment />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Create"
+                  classes={{
+                    primary: classes.item
+                  }}
+                />
+              </ListItem>
+              <ListItem button onClick={() => this.changePageNumber(3)}>
+                <ListItemIcon
+                  classes={{
+                    root: classes.item
+                  }}
+                >
+                  <CloudUpload />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Develop"
+                  classes={{
+                    primary: classes.item
+                  }}
+                />
+              </ListItem>
+              <ListItem button onClick={() => this.changePageNumber(4)}>
+                <ListItemIcon
+                  classes={{
+                    root: classes.item
+                  }}
+                >
+                  <HelpIcon />
+                </ListItemIcon>
+                <ListItemText
+                  primary="Contact"
+                  classes={{
+                    primary: classes.item
+                  }}
+                />
+              </ListItem>
+            </List>
+          </Drawer>
+          <main className={classes.content}>
+            {pageNumber === 1 && <Home />}
+            {pageNumber === 2 && <Create />}
+            {pageNumber === 3 && <Develop />}
+            {pageNumber === 4 && <Contact />}
+          </main>
+        </div>
       </div>
     );
   }
--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-import { colors } from '../../config';
+import React, { Component } from "react";
+import styled from "styled-components";
+import { colors } from "../../config";
 
-import { Typography } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { Typography } from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+import HEADER from "./components/header.jpg";
+
+const HeaderImg = styled.img`
+  width: 100%;
+  height: 250px;
+  object-fit: cover;
+`;
+
+const CenterDiv = styled.div`
+  width: 100%;
+  text-align: center;
+`;
+
+const TextDiv = styled.div`
+  padding-left: 150px;
+  padding-right: 150px;
+  padding-top: 15px;
+`;
+
+const H3 = styled.h3`
+  color: ${colors.primary};
+`;
 
 const styles = theme => ({
   root: {
-    display: 'flex'
+    display: "flex"
   }
 });
 
@@ -16,7 +39,21 @@ class Create extends Component {
     const { classes } = this.props;
     return (
       <div>
-      <h1>Lets make some shit</h1>
+        <CenterDiv>
+          <HeaderImg src={HEADER} />
+        </CenterDiv>
+        <TextDiv>
+          <H3>Where to start?</H3>
+        </TextDiv>
+        <TextDiv>
+          <H3>Small Buisness</H3>
+        </TextDiv>
+        <TextDiv>
+          <H3>Startup</H3>
+        </TextDiv>
+        <TextDiv>
+          <H3>Services</H3>
+        </TextDiv>
       </div>
     );
   }
