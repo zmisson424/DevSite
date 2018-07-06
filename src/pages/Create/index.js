5b0ef1b979516d2789084578be227cdff6a20c0d
zmisson424 <zmisson424@gmail.com>
2018-07-05 22:09:51 -0400

    added content

--- src/pages/Create/index.js
+++ src/pages/Create/index.js
@@ -1,11 +1,14 @@
-import React, { Component } from "react";
-import styled from "styled-components";
-import { colors } from "../../config";
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { colors } from '../../config';
 
-import { Typography } from "@material-ui/core";
-import { withStyles } from "@material-ui/core/styles";
+import { Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
 
-import HEADER from "./components/header.jpg";
+import HEADER from './components/header.jpg';
+import SMALL_BUSINESS from './components/small-business.jpg';
+import STARTUP from './components/startup.jpg';
+import SERVICES from './components/services.jpg';
 
 const HeaderImg = styled.img`
   width: 100%;
@@ -24,13 +27,15 @@ const TextDiv = styled.div`
   padding-top: 15px;
 `;
 
-const H3 = styled.h3`
-  color: ${colors.primary};
-`;
-
 const styles = theme => ({
   root: {
-    display: "flex"
+    display: 'flex'
+  },
+  header: {
+    color: colors.primary
+  },
+  body: {
+    marginBottom: 50
   }
 });
 
@@ -43,16 +48,60 @@ class Create extends Component {
           <HeaderImg src={HEADER} />
         </CenterDiv>
         <TextDiv>
-          <H3>Where to start?</H3>
+          <Typography className={classes.header} variant="display2">
+            Where to start?
+          </Typography>
+          <Typography className={classes.body} variant="display1">
+            There are lots of different development techniques that will create
+            the perfect setup for you company or business.
+          </Typography>
         </TextDiv>
+        <CenterDiv>
+          <HeaderImg src={SMALL_BUSINESS} />
+        </CenterDiv>
         <TextDiv>
-          <H3>Small Buisness</H3>
+          <Typography className={classes.header} variant="display2">
+            Small Business
+          </Typography>
+          <Typography className={classes.body} variant="display1">
+            For small business trying to make a space for themselves in the
+            world, it can be best to use Shopify or Squarespace. Embark can help
+            launch and create a site that perfectly matches your vision. Then it
+            will be easy for you to maintain and update your site with out the
+            need of a developer. Simple and intuitive.
+          </Typography>
         </TextDiv>
+        <CenterDiv>
+          <HeaderImg src={STARTUP} />
+        </CenterDiv>
         <TextDiv>
-          <H3>Startup</H3>
+          <Typography className={classes.header} variant="display2">
+            Startup
+          </Typography>
+          <Typography className={classes.body} variant="display1">
+            Do you have a great idea and want to launch your own tech service on
+            the web, mobile application or both, but might not know where to
+            start? A fresh build might be the solution. Using modern frameworks
+            such as React or Angular can help speed up the development process
+            for your Web App. Using Android Studio a new Android Application can
+            also be built from the ground up to do exactyl as you envision.
+          </Typography>
         </TextDiv>
+        <CenterDiv>
+          <HeaderImg src={SERVICES} />
+        </CenterDiv>
         <TextDiv>
-          <H3>Services</H3>
+          <Typography className={classes.header} variant="display3">
+            Services
+          </Typography>
+          <Typography className={classes.body} variant="display1">
+          If you already have a websiter or application, but are just looking
+          for some new additions, this can be done as well. For example, local
+          churches might look to update their website to support live
+          streaming to broadcast Sunday service. They might also want to add a
+          spot to accept donations through a credit card. These small
+          imporvements are always possible.
+          </Typography>
         </TextDiv>
       </div>
     );
