7451e6b3231341e44157a6ebd5e06d6daaa3b73e
Zach <zachery@justspeakeasy.com>
2020-09-25 08:31:54 -0500

    basic about section design

--- src/views/About/index.jsx
+++ src/views/About/index.jsx
@@ -8,8 +8,40 @@ const useStyles = makeStyles({
     width: "100%",
     minHeight: "100vh",
     display: "flex",
-    justifyContent: "start",
+    flexDirection: "column",
+    alignItems: "end",
+    justifyContent: "center"
+  },
+  header: {
+    marginBottom: 48
+  },
+  summary: {
+    color: MyColors.white
+  },
+  section: {
+    width: "75%",
+    display: "flex",
     alignItems: "center"
+  },
+  image: {
+    width: 200,
+    height: 250,
+    objectFit: "cover",
+    borderRadius: 10,
+    mixBlendMode: "multiply",
+    "&:hover": {
+      mixBlendMode: "normal"
+    }
+  },
+  imageWrapper: {
+    width: 200,
+    height: 250,
+    backgroundColor: MyColors.secondary,
+    borderRadius: 10,
+    boxShadow: `-10px 10px 10px  ${MyColors.primary}`
+  },
+  imageBox: {
+    marginRight: 48
   }
 });
 
@@ -18,9 +50,29 @@ function About() {
 
   return (
     <div className={classes.root}>
-      <Typography variant="h6" color="secondary">
+      <Typography variant="h3" color="secondary" className={classes.header}>
         About
       </Typography>
+      <div className={classes.section}>
+        <div className={classes.imageBox}>
+          <div className={classes.imageWrapper}>
+            <img
+              className={classes.image}
+              src="https://res.cloudinary.com/jerrick/image/upload/fl_progressive,q_auto,w_1024/mw3sd1epiw7tsejmfqhg.jpg"
+            />
+          </div>
+        </div>
+        <Typography className={classes.summary}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed
+          pulvinar proin gravida hendrerit lectus a. Urna id volutpat lacus
+          laoreet non curabitur gravida. Dapibus ultrices in iaculis nunc. Dui
+          ut ornare lectus sit. Odio facilisis mauris sit amet massa. Diam quis
+          enim lobortis scelerisque fermentum dui faucibus. Placerat duis
+          ultricies lacus sed turpis tincidunt id aliquet. Ipsum consequat nisl
+          vel pretium lectus quam id. In est ante in nibh.
+        </Typography>
+      </div>
     </div>
   );
 }
